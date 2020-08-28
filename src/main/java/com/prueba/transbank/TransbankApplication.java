38e49e7ca5c3840fe03093489c329b2cae0a5752
Hector Varela <hector.varela@latam.com>
2020-08-28 19:46:12 -0400

    agrega base del proyecto

---
+++ src/main/java/com/prueba/transbank/TransbankApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.transbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/transbank/TransbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.transbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
