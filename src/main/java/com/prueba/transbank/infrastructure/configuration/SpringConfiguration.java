4bcb08a2ae72efc7e2e788ef91bd744482ebbf36
Hector Varela <hector.varela@latam.com>
2020-08-28 20:12:42 -0400

    agrega estructura del proyecto

---
+++ src/main/java/com/prueba/transbank/infrastructure/configuration/SpringConfiguration.java
@@ -0,0 +1,8 @@
+package com.prueba.transbank.infrastructure.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+
+}
---
+++ src/main/java/com/prueba/transbank/infrastructure/entrypoints/rest/Controller.java
@@ -0,0 +1,8 @@
+package com.prueba.transbank.infrastructure.entrypoints.rest;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+}
