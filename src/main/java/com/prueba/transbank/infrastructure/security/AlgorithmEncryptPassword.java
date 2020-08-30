e1476b69968f273f66e2b1c2db2d302f575255ac
Hector Varela <hector.varela@latam.com>
2020-08-30 14:17:32 -0400

    add test para AlgorithmEncryptPasswordTest

--- src/main/java/com/prueba/transbank/infrastructure/security/AlgorithmEncryptPassword.java
+++ src/main/java/com/prueba/transbank/infrastructure/security/AlgorithmEncryptPassword.java
@@ -16,28 +16,37 @@ import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 import java.util.Base64;
 
-@Repository
+@Component
 public class AlgorithmEncryptPassword implements AlgorithmEncryptPasswordDataProvide {
 
     private Logger logger = LoggerFactory.getLogger(AlgorithmEncryptPassword.class);
 
 
-    @Value("${algorithmEncryptpassword.salt}")
-    private  String  SALT;
-    @Value("${algorithmEncryptpassword.keylength}")
-    private  int KEY_LENGTH;
-    @Value("${algorithmEncryptpassword.iterations}")
-    private  int ITERATIONS;
-    @Value("${algorithmEncryptpassword.algorithm}")
-    private String ALGORITHM;
 
-    public AlgorithmEncryptPassword(){}
+    private  String  salt;
+
+    private  int keyLength;
+
+    private  int iteration;
+
+    private String algorithm;
+
+    public AlgorithmEncryptPassword(  @Value("${algorithmEncryptpassword.salt}") String salt,
+                                      @Value("${algorithmEncryptpassword.keylength}") int keyLength,
+                                      @Value("${algorithmEncryptpassword.iterations}") int iteration,
+                                      @Value("${algorithmEncryptpassword.algorithm}") String algorithm
+                                      ){
+        this.salt = salt;
+        this.keyLength = keyLength;
+        this.iteration = iteration;
+        this.algorithm = algorithm;
+    }
 
     private  byte[] hash(char[] password) {
-        PBEKeySpec spec = new PBEKeySpec(password,  SALT.getBytes(), ITERATIONS, KEY_LENGTH);
+        PBEKeySpec spec = new PBEKeySpec(password,  salt.getBytes(), iteration, keyLength);
         Arrays.fill(password, Character.MIN_VALUE);
         try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance(ALGORITHM);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm);
             return skf.generateSecret(spec).getEncoded();
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
---
+++ src/test/unitTest/java/com/prueba/transbank/infrastructure/security/AlgorithmEncryptPasswordTest.java
@@ -0,0 +1,39 @@
+package com.prueba.transbank.infrastructure.security;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(locations = "classpath:secrets/secret.properties")
+public class AlgorithmEncryptPasswordTest {
+
+    AlgorithmEncryptPassword algorithmEncryptPassword;
+
+    @Value("${algorithmEncryptpassword.salt}")
+    private String salt;
+    @Value("${algorithmEncryptpassword.keylength}")
+    private int keyLength;
+    @Value("${algorithmEncryptpassword.iterations}")
+    private int iteration;
+    @Value("${algorithmEncryptpassword.algorithm}")
+    private String algorithm;
+
+    @Before
+    public  void init(){
+        algorithmEncryptPassword = new AlgorithmEncryptPassword( salt, keyLength, iteration, algorithm);
+    }
+
+    @Test
+    public void generateSecurePasswordTest() {
+
+        String pass = algorithmEncryptPassword.generateSecurePassword("tito");
+
+        assertTrue( pass != null && pass.length()>30);
+    }
+}
